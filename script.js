16e8dcb738b817eb448f5e154a4b573a85ec9ae5
humayunahmed8 <developer.humayun@gmail.com>
2024-06-30 17:10:18 +0600

    Log all elements in the document and iterate over them, logging each one

--- script.js
+++ script.js
@@ -6,4 +6,11 @@ const documentHref = document.location.href;
 const element = document.getElementById("link");
 element.href = documentHref;
 
+// Log all elements in the document and iterate over them, logging each one
+console.log(document.all);
+
+for (x of document.all) {
+  console.log(x);
+}
+
 
