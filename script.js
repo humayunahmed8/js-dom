6230405f97eed5a9a473f5441cf07cc8a570ddb6
humayunahmed8 <developer.humayun@gmail.com>
2024-06-30 18:28:16 +0600

    Styling updates using JavaScript
    - Set font weight to bold for element with ID 'heading'.
    - Set text color to green for all elements with class 'item'.
    - Set text color to red for the last child element with class 'item'.

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
-// Get all <li> elements
-const element = document.getElementsByTagName("li");
+// Set font weight to bold for element with ID 'heading'.
+const heading = document.querySelector("#heading");
+heading.style.fontWeight = 'bold';
 
-// Loop through each <li> element
-for(let i=0; i<element.length; i++) {
-  element[i].style.color = "red";
+// Set text color to green for all elements with class 'item'
+const item = document.querySelectorAll(".item");
+
+for(let i of item) {
+  i.style.color = 'green';
+}
+
+// Set text color to red for the last child element with class 'item'."
+const lastItem = document.querySelectorAll(".item:last-child");
+
+for(let item of lastItem) {
+  item.style.color = 'red';
 }
 
