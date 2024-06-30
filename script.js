e617daac013ec6a49f48785d9b3559c5eb55cc7b
humayunahmed8 <developer.humayun@gmail.com>
2024-06-30 18:04:03 +0600

    Set text color to red for all <li> elements
    - Retrieve all <li> elements using document.getElementsByTagName('li').
    - Loop through each <li> element and apply red text color.

--- script.js
+++ script.js
@@ -1,18 +1,8 @@
-// Get the element with the ID "list"
-const itemContainer = document.getElementById("list");
+// Get all <li> elements
+const element = document.getElementsByTagName("li");
 
-// Get all elements with the class "item" within the "list" container
-const items = itemContainer.getElementsByClassName("item");
-
-// Log each element with the class "item"
-for (x of items) {
-  console.log(x);
+// Loop through each <li> element
+for(let i=0; i<element.length; i++) {
+  element[i].style.color = "red";
 }
-
-// Loop through each "item" element and apply red text color
-for (let i = 0; i < items.length; i++) {
-  // Set the text color of each element to red
-  items[i].style.color = 'red';
-}
-
 
