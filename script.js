e879e0818ded2e3ad0891a5b31cd1bfb89b461ad
humayunahmed8 <developer.humayun@gmail.com>
2024-07-01 18:08:52 +0600

    Creating DOM Element

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
-// Select the next sibling of the element with class "item" and change its color to red
-// Select the previous sibling of the next sibling and change its color to green
-const childrenTwo = document.querySelector(".item").nextElementSibling;
-const childrenOne = childrenTwo.previousElementSibling;
-childrenTwo.style.color = "red";
-childrenOne.style.color = "green";
+// Creating an element
+const divElement = document.createElement("div");
+divElement.className = "red";
+divElement.setAttribute("title", "red");
+divElement.textContent = "This is heading";
+const container = document.querySelector("#list-items");
+const heading = container.querySelector("#heading");
+container.insertBefore(divElement, heading);
 
 
