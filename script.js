e0dc7f4747c7c2e907e2292e5e387ed7c9d63508
humayunahmed8 <developer.humayun@gmail.com>
2024-07-01 17:40:07 +0600

    Retrieve parent element of the .list class element

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
 // Parent Child Relation 
-const grandparent = document.querySelector(".list-item");
-// const parent = grandparent.children;
-// const children = parent[1].children;
-const children = grandparent.querySelectorAll(".item");
+const children = document.querySelector(".list");
+const parent = children.parentElement;
 
-console.log(children);
+console.log(parent);
 
