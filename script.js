d2022b91d40a2bb8ff0173ad89a0c52caa372018
humayunahmed8 <developer.humayun@gmail.com>
2024-07-01 17:49:49 +0600

    next sibling and previous sibling

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
-// Parent Child Relation 
-const children = document.querySelector(".list");
-const grandparent = children.closest(".container");
+// Select the next sibling of the element with class "item" and change its color to red
+// Select the previous sibling of the next sibling and change its color to green
+const childrenTwo = document.querySelector(".item").nextElementSibling;
+const childrenOne = childrenTwo.previousElementSibling;
+childrenTwo.style.color = "red";
+childrenOne.style.color = "green";
 
-console.log(grandparent);
 
